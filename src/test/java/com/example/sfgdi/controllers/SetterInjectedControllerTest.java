fb848d9f62a24595a9b905953cc95c4b450ecb99
Sanja.Tashkovska <sanja.tashkovska@seavus.com>
2023-02-09 15:00:38 +0100

    First commit

---
+++ src/test/java/com/example/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.sfgdi.controllers;
+
+import com.example.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController setterInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreetingService() {
+        System.out.println(setterInjectedController.getGreeting());
+    }
+}
